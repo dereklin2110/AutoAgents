d62fc838e3d17f9e38f738bc594f1f2fda43b679
dereklin <dereklin2110@gmail.com>
2024-05-19 23:15:53 +0800

    ss

--- autoagents/system/provider/openai_api.py
+++ autoagents/system/provider/openai_api.py
@@ -196,10 +196,10 @@ class OpenAIGPTAPI(BaseGPTAPI, RateLimiter):
 
     async def _achat_completion_stream(self, messages: list[dict]) -> str:
         print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-        print(messages)
+        # print(messages)
         user_content = [item['content'] for item in messages if item['role'] == 'user']
 
-        url = "http://0.0.0.0:11434/api/generate"
+        url = "http://localhost:11434/api/generate"
         api = OpenAIGPTAPI(url)
         payload = {
             "model": "phi3",
@@ -212,7 +212,7 @@ class OpenAIGPTAPI(BaseGPTAPI, RateLimiter):
         }
 
         response = await requests.request("POST", url, headers=headers, data=json.dumps(payload))
-
+        print(response)
         return response
 
     def _cons_kwargs(self, messages: list[dict]) -> dict:
