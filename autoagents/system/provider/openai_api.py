5239c764c8eefe86b24f6cf3ad1a91381b44d8cd
dereklin <dereklin2110@gmail.com>
2024-05-21 01:45:21 +0800

    ss

--- autoagents/system/provider/openai_api.py
+++ autoagents/system/provider/openai_api.py
@@ -214,7 +214,7 @@ class OpenAIGPTAPI(BaseGPTAPI, RateLimiter):
 
     async def _achat_completion_stream(self, messages: list[dict]) -> str:
         print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-        # print(messages)
+        print(messages)
 
         # user_content = [item['content'] for item in messages if item['role'] == 'user']
 
@@ -236,10 +236,10 @@ class OpenAIGPTAPI(BaseGPTAPI, RateLimiter):
             {"role": "user", "content": "why is the sky blue?"}
         ]
         response = await self.chat_with_ollama(model, aaa)
-        print(json.dumps(response, indent=4))
+        #print(json.dumps(response, indent=4))
 
         # response = await requests.request("POST", url, headers=headers, data=json.dumps(payload))
-        print(response)
+        #print(response)
         return response
 
     def _cons_kwargs(self, messages: list[dict]) -> dict:
